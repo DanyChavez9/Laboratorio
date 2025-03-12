361df0940217249bcaea0fcb804feea09a7ec758
Andy <148731790+DanyChavez9@users.noreply.github.com>
2025-03-11 18:16:39 -0600

    Add files via upload
    Script para base de datos

---
+++ Script/SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE Clients (
+id INT NOT NULL PRIMARY KEY IDENTITY,
+name VARCHAR (100) NOT NULL,
+email VARCHAR (150) NOT NULL UNIQUE,
+phone VARCHAR (20) NULL,
+address VARCHAR (100) NULL,
+created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO Clients (name, email, phone, address)
+VALUES
+('Bill Gates', 'billgates@microsoft.com', '+123456789', 'New York, USA'),
+('Elon Musk', 'elonmusk@spacex.com', '+1122334455', 'Florida, USA');
